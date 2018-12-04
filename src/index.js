d3a09717bbee2564421d2d2ab1e56b108bb170a6
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2018-12-04 14:38:39 +0100

    Started using redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import './Style/styles.scss';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import AppRouter from './routers/AppRouter';
+// import './Style/styles.scss';
 
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+// ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+
+import {createStore} from 'redux';
+
+const store = createStore((state = {count:0}) => {
+    return state;
+});
+
+console.log(store.getState());

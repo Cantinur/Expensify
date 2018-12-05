c685f52905d5962a1551cca1fb926511283fc192
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2018-12-05 18:19:04 +0100

    Learing Redux

--- src/playgound/redux101.js
+++ src/playgound/redux101.js
@@ -14,9 +14,13 @@ const decrementCount = ({decrementBy = 1} = {}) =>({
 const setCount = ({setTo = 0} = {}) =>({
     type: 'SET',
     setTo
-});
+})
 
-const store = createStore((state = {count:0}, action) => {
+//Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+const countReducer = (state = {count:0}, action) => {
     switch (action.type){
         case 'INCREMENT':
             return {
@@ -33,7 +37,9 @@ const store = createStore((state = {count:0}, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 /*const unsubscribe =*/ store.subscribe(() => {
     console.log(store.getState());

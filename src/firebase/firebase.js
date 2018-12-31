24676d67eb5605cc1a1ade4092530ebc2c5c3a0d
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2018-12-31 11:54:08 +0100

    Learn about snapshots and storing in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase';
-import { timeout } from 'q';
 
 // Initialize Firebase
 const config = {
@@ -14,27 +13,70 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-const dataOn = database.ref().on('value', snapshot => {
-    console.log(`${snapshot.val().name} is a ${snapshot.val().jobb.title} at ${snapshot.val().jobb.company}`);
-}, e => {
-    console.log('Error accured', e);
+//child_removed
+database.ref('expenses').on('child_removed', snapshot => {
+    console.log(snapshot.key, snapshot.val());
 });
 
-setTimeout(()=> {
-    database.ref('jobb').update({
-        company: 'Google'
-    })
-}, 3500);
+database.ref('expenses').on('child_changed', snapshot => {
+    console.log(snapshot.key, snapshot.val());
+});
 
-setTimeout(()=>{
-    database.ref().off('value', dataOn);
-}, 7000);
+database.ref('expenses').on('child_added', snapshot => {
+    console.log(snapshot.key, snapshot.val());
+})
 
-setTimeout(()=>{
-    database.ref('jobb').update({
-        company: 'Apple'
-    })
-}, 10000);
+// database.ref('expenses').on('value', snapshot => {
+//     const expenseArray = [];
+//     snapshot.forEach(childSnapshot=> {
+//         expenseArray.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenseArray);
+// });
+
+
+//expenses with three items
+// const expense1 = {
+//     description:'Airplain tickets',
+//     note: 'Back to Oslo after wisiting Meea',
+//     amount: 200000,
+//     createdAt: 1546252117699,
+// };
+// database.ref('expenses').push(expense1);
+
+
+// database.ref('notes/-LV2mBaByhXCdb2Jpp68').remove()
+
+
+// database.ref('notes').push({
+//     title: 'Learn somthing new',
+//     body: 'I should learn more code'
+// });
+
+// const dataOn = database.ref().on('value', snapshot => {
+//     console.log(`${snapshot.val().name} is a ${snapshot.val().jobb.title} at ${snapshot.val().jobb.company}`);
+// }, e => {
+//     console.log('Error accured', e);
+// });
+
+// setTimeout(()=> {
+//     database.ref('jobb').update({
+//         company: 'Google'
+//     })
+// }, 3500);
+
+// setTimeout(()=>{
+//     database.ref().off('value', dataOn);
+// }, 7000);
+
+// setTimeout(()=>{
+//     database.ref('jobb').update({
+//         company: 'Apple'
+//     })
+// }, 10000);
 
 
 // database.ref('location').once('value')
